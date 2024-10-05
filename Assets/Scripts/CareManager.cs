695e2cc44014ebd11ac549f328f909887cb93014
Eric Oudin <Eric_Oudin29@yahoo.com>
2024-10-04 22:21:56 -0400

    Scriptable object creature added

--- Assets/Scripts/CareManager.cs
+++ Assets/Scripts/CareManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CareManager : MonoBehaviour
 {
+    // List of all tiny creatures you own
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/CreatureStats.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+public class CreateCreature
+{
+    [MenuItem("Assets/Create/Creature")]
+    public static void Create()
+    {
+        CreatureStats ati = ScriptableObject.CreateInstance<CreatureStats>();
+        AssetDatabase.CreateAsset(ati, "Assets/ScriptableObjects/Creatures/NewCreature.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = ati;
+    }
+}
+
+public class CreatureStats : ScriptableObject
+{
+    public Type CreatureType;
+
+    public int HitPoints;
+
+    public int Defence;
+
+    public int Attack;
+
+    public int Hunger;
+
+    public int Entertainment;
+
+    public int Hygiene;
+
+    public Animator Animator;
+}
